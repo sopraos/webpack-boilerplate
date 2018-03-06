17f151138968de57f247083ce3081b00e0339a38
Sopraos <sopraos@outlook.fr>
2018-03-06 06:31:02 +0100

    Init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// Webpack configuration
+// ===\===================
+
+const path = require('path');
+
+module.exports = env => {
+	const isDevelopment = env.development === true;
+
+	let config = {
+	};
+
+	return config;
+};
